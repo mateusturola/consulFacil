285ff0bd18262d1238835d5a5ef10db02090f724
mateusturola <turolamateus@gmail.com>
2022-09-03 00:17:23 -0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 5000,
+    port: 3030
+  },
+  preview: {
+    port: 8080
   },
   plugins: [react(), tsconfigPaths()]
 })
